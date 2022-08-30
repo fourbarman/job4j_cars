049dedb538074c906d801b594a8388b73e9535d1
fourbarman <maks.java@yandex.ru>
2022-08-30 10:11:08 +0400

    Liquibase schema create script, properties added.

---
+++ db/scripts/001_ddl_init_schema.sql
@@ -0,0 +1,24 @@
+/* Author: fourbarman */
+
+/*
+Создание таблиц.
+*/
+
+CREATE TABLE IF NOT EXISTS auto_user
+(
+    id       SERIAL PRIMARY KEY,
+    login    VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    CONSTRAINT login_unique UNIQUE (login)
+);
+
+CREATE TABLE IF NOT EXISTS auto_post
+(
+    id           SERIAL PRIMARY KEY,
+    text         INT NOT NULL,
+    created      INT NOT NULL,
+    auto_user_id INT NOT NULL
+);
+
+
+
